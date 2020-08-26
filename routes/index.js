3d0322edc5bf3322ec5c1f6689acebdccad1db52
Abhinandan M.S <59559188+Abhinandan-pixel@users.noreply.github.com>
2020-08-26 18:22:45 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -4,71 +4,101 @@ var router = express.Router();
 var passport = require("passport");
 var user = require("../models/user")
 var campground = require("../models/campground");
+var comment = require("../models/comments");
 var middleware = require("../middleware/index");
-router.get("/",(req, res)=>{
+router.get("/", (req, res) => {
     res.render("landingpage")
 })
 
-router.get("/register",(req, res)=>{
+router.get("/register", (req, res) => {
     res.render("register");
 })
 //Sign up handler.
-router.post("/register",(req, res)=>{
+router.post("/register", (req, res) => {
     let newUser = new user(
         {
-           username : req.body.username,
-           firstName: req.body.firstName,
-           lastName : req.body.lastName,
-           email    : req.body.email,
-           avatar   : req.body.avatar,
-           about    : req.body.about,
+            username: req.body.username,
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            email: req.body.email,
+            avatar: req.body.avatar,
+            about: req.body.about,
         })
-        if(req.body.admin === 'claireDunphy#23')
-            newUser.isAdmin = true;
-    user.register(newUser,req.body.password,(err,user)=>{
-        if(err){
-            req.flash("error",err.message);
+    if (req.body.admin === 'claireDunphy#23')
+        newUser.isAdmin = true;
+    user.register(newUser, req.body.password, (err, user) => {
+        if (err) {
+            req.flash("error", err.message);
             res.redirect("/register")
         }
-        passport.authenticate("local")(req,res, ()=>{
-            req.flash("success",`welcome aboard ${user.username}`)
+        passport.authenticate("local")(req, res, () => {
+            req.flash("success", `welcome aboard ${user.username}`)
             res.redirect("/campgrounds");
         })
 
     });
 });
 //Login handler.
-router.get("/login",(req, res)=>{
+router.get("/login", (req, res) => {
     res.render("login");
 })
 //app.post("/login",middleware,callback).
-router.post("/login",passport.authenticate("local",
+router.post("/login", passport.authenticate("local",
     {
-        successRedirect:"/campgrounds",
-        failureRedirect:"/login",
+        successRedirect: "/campgrounds",
+        failureRedirect: "/login",
         failureFlash: true,
-    }), (req, res)=>{
-        failureFlash: req.flash("error","Invalid username or password.")
+    }), (req, res) => {
+        failureFlash: req.flash("error", "Invalid username or password.")
     })
 //Logout ROUTE.
-router.get("/logout",function(req, res){
+router.get("/logout", function (req, res) {
     req.logOut();
-    req.flash("success","Aloha, Come back soon.");
+    req.flash("success", `Aloha, Come back soon.`);
     res.redirect("/campgrounds")
 })
+
 // Users profile.
-router.get("/users/:id",middleware.isLoggedIn,(req, res)=>{
-    user.findById(req.params.id,(error,profile)=>{
-        if(error){
-            req.flash("error","Profile Not Found");
+router.get("/users/:id", middleware.isLoggedIn, (req, res) => {
+    user.findById(req.params.id, (error, profile) => {
+        if (error) {
+            req.flash("error", "Profile Not Found");
             req.redirect("/");
-        }else{
-            campground.find().where('author.id').equals(profile._id).exec((err,campgrounds)=>{
-                res.render("users/show",{profile:profile,campgrounds:campgrounds});
+        } else {
+            campground.find().where('author.id').equals(profile._id).exec((err, campgrounds) => {
+                user.find({}, (err, users) => {
+                    if (err) {
+                        console.log("error")
+                    } else {
+                        res.render("users/show", { profile: profile, campgrounds: campgrounds, users: users });
+                    }
+                })
+
             })
-            
+
         }
     })
 })
+//User profile destroy.
+router.delete("/users/:id", (req, res) => {
+    comment.deleteMany({ "author.id": req.params.id }, (err) => {
+        if (err) {
+            req.flash("error","Unsuccessful,try again later,Sorry for inconvenience");
+            req.redirect("/");
+        } else {
+            campground.deleteMany({ "author.id": req.params.id }, (err) => {
+                if (err) {
+                    req.flash("error","Unsuccessful,try again later,Sorry for inconvenience");
+                    req.redirect("/");
+                } else {
+                    user.findByIdAndRemove(req.params.id, (err) => {
+                        req.flash("success","Successfully removed")
+                        res.redirect("back");
+                    })
+                }
+            })
+        }
 
+    })
+})
 module.exports = router;
